053de00f39f99fcfc88be709add0c8b6a21ef146
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-02-10 09:09:35 +0000

    deploy: 18043cd360fd27b9b5fbaa5b6d573ea6bb53d5b9

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+const header = document.querySelector('header.header');
+
+if (header) {
+
+  const burger = header.querySelector('.header__burger');
+
+  burger.addEventListener('click', function() {
+    this.classList.toggle('js-menu-active');
+  })
+
+}

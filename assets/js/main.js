f81347338771006425b178b199a62ee54badba54
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-02-12 20:47:32 +0000

    deploy: ac27b92e0ec8152247052a677b74c82ea2204ca9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 
+const disclaimerClosing = document.querySelector('.disclaimer.disclaimer--closing');
+
+const disclaimerClose = disclaimerClosing.querySelector('.disclaimer__close');
+
+disclaimerClose.addEventListener('click', () => {
+  disclaimerClosing.classList.add('js-close');
+})
+
 const header = document.querySelector('header.header');
 
 if (header) {
@@ -8,5 +16,76 @@ if (header) {
   burger.addEventListener('click', function() {
     this.classList.toggle('js-menu-active');
   })
+
+}
+
+
+{
+
+  const portfolioSwiper = document.querySelector('.hero__slider');
+
+  if (portfolioSwiper) {
+
+    let mySwiper = new Swiper(portfolioSwiper, {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.slider-nav__next',
+        prevEl: '.slider-nav__prev',
+      },
+
+      uniqueNavElements: true,
+
+      // Бесконечная прокрутка
+      //loop: true,
+
+      // Откл функционала, если слайдов меньше, чем нужно
+      watchOverflow: true,
+
+
+      // Отступ между слайдами
+      //spaceBetween: 40,
+
+      // Стартовый слайд
+      initialSlide: 0,
+
+      effect: 'coverflow',
+      centeredSlides: true,
+      grabCursor: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+
+      // Брейк поинты (адаптив)
+      // Ширина экрана
+      // breakpoints: {
+      //   320: {
+      //     slidesPerView: 2,
+      //     spaceBetween: 12,
+      //   },
+      //   768: {
+      //     slidesPerView: 3,
+      //     spaceBetween: 20,
+      //   },
+      //
+      //   1100: {
+      //     spaceBetween: 40,
+      //   },
+      // }
+    });
+
+  }
+
+
+
+
+
 
 }

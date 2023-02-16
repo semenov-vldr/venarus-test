c8896c1d147177bceff4a036434468df7d8a21a0
Vladimir <vova2066@yandex.ru>
2023-02-16 09:44:19 +0300

    start block video

---
+++ src/components/opinion/opinion.js
@@ -0,0 +1,39 @@
+
+
+
+
+  const slider = document.querySelector('.opinion__slider');
+
+if (slider) {
+
+
+  let mySwiper = new Swiper(slider, {
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+
+    uniqueNavElements: true,
+
+    slidesPerView: 1,
+
+    // Бесконечная прокрутка
+    loop: true,
+
+    // Откл функционала, если слайдов меньше, чем нужно
+    watchOverflow: true,
+
+    //centeredSlides: true,
+
+    // Отступ между слайдами
+    spaceBetween: 16,
+
+
+  });
+
+}
+
+
+
+
